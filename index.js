4f21635e3190d3c2cde3c1d223e2872772135310
AHMET UĞUR <a.ugur8206@gmail.com>
2024-11-28 21:33:10 +0300

    ekleme

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 document
-  .getElementById("appointment-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Randevunuz alınmıştır. Teşekkürler!");
+  .getElementById("randevu-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Formun varsayılan davranışını engelle
+
+    let ad = document.getElementById("ad").value;
+    let email = document.getElementById("email").value;
+    let telefon = document.getElementById("telefon").value;
+    let tarih = document.getElementById("tarih").value;
+    let saat = document.getElementById("saat").value;
+    let islemler = document.getElementById("islemler").value;
+    let mesaj = document.getElementById("mesaj").value;
+
+    // Randevu bilgilerini konsola yaz
+    console.log(`Randevu Bilgileri:  
+      Ad: ${ad}  
+      Email: ${email}  
+      Telefon: ${telefon}  
+      Tarih: ${tarih}  
+      Saat: ${saat}  
+      Yapılacak İşlem: ${islemler}  
+      Mesaj: ${mesaj}`);
+
+    alert("Randevunuz alınmıştır!"); // Kullanıcıya randevunun alındığını bildir
   });
